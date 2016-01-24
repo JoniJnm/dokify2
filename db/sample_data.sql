b440602db3acd216a5826722d5a54298cc7c5c55
JoniJnm <test@example.org>
2016-01-24 17:51:05 +0100

    Fix sample data order

--- db/sample_data.sql
+++ db/sample_data.sql
@@ -1,17 +1,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-INSERT INTO `agreements` (`id`, `name`) VALUES
-(9, 'Acuerdo 1'),
-(11, 'Acuerdo 2');
-
-INSERT INTO `agreement_relations` (`id_agreement`, `id_relation`, `order`) VALUES
-(9, 22, 1),
-(11, 17, 1),
-(9, 18, 2),
-(11, 16, 2),
-(9, 23, 3);
-
 INSERT INTO `companies` (`id`, `name`) VALUES
 (8, 'Empresa A'),
 (6, 'Empresa B'),
@@ -28,3 +17,14 @@ INSERT INTO `relations` (`id`, `client`, `provider`) VALUES
 (16, 8, 1),
 (18, 8, 6),
 (21, 15, 8);
+
+INSERT INTO `agreements` (`id`, `name`) VALUES
+(9, 'Acuerdo 1'),
+(11, 'Acuerdo 2');
+
+INSERT INTO `agreement_relations` (`id_agreement`, `id_relation`, `order`) VALUES
+(9, 22, 1),
+(11, 17, 1),
+(9, 18, 2),
+(11, 16, 2),
+(9, 23, 3);
